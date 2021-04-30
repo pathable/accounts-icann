d05947bbc2e1c2d7ae460189b14d79f518da5fd9
Igor Fontana <igor193@gmail.com>
2021-04-30 09:12:45 -0300

    ICANN Authorization

--- icann.js
+++ icann.js
@@ -1,7 +1,7 @@
 Accounts.oauth.registerService("icann");
 
 if (Meteor.isClient) {
-  const loginWithOidc = (options, callback) => {
+  const loginWithIcann = (options, callback) => {
     // support a callback without options
     if (!callback && typeof options === "function") {
       callback = options;
@@ -13,8 +13,8 @@ if (Meteor.isClient) {
     );
     Oidc.requestCredential(options, credentialRequestCompleteCallback);
   };
-  Accounts.registerClientLoginFunction("icann", loginWithOidc);
-  Meteor.loginWithOidc = (...args) => Accounts.applyLoginFunction("icann", args);
+  Accounts.registerClientLoginFunction("icann", loginWithIcann);
+  Meteor.loginWithIcann = (...args) => Accounts.applyLoginFunction("icann", args);
 } else {
   Accounts.addAutopublishFields({
     forLoggedInUser: ["services.icann"],
